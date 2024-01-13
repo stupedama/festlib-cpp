fdfb8a202dc01ffc096f6247645fda1729c3f5ae
Fredrik Fjeldvær <fredrik@ffj.no>
2024-01-13 17:26:51 +0100

    alias for std::vector

---
+++ include/container.h
@@ -0,0 +1,16 @@
+#ifndef CONTAINER_H_
+#define CONTAINER_H_
+
+#include <vector>
+
+namespace festlib
+{
+
+  // default container used
+
+  template<typename T>
+  using Container = std::vector<T>;
+
+} // namespace
+
+#endif
