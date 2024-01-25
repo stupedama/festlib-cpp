314aeabe1ce38ffe407cbbbfa11a52533570bcc6
Fredrik Fjeldvær <fredrik@ffj.no>
2024-01-25 22:04:57 +0100

    changed from unordered_map to map

--- include/container.h
+++ include/container.h
@@ -9,14 +9,14 @@
 #ifndef CONTAINER_H_
 #define CONTAINER_H_
 
-#include <unordered_map>
+#include <map>
 #include <vector>
 
 namespace festlib {
 // default container used
 
 template <typename T> using Container = std::vector<T>;
-template <typename T, typename U> using Map = std::unordered_map<T, U>;
+template <typename T, typename U> using Map = std::map<T, U>;
 
 } // namespace festlib
 
